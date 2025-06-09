54b2e17249ea63d4bdc0385938cb126ffac96346
SarahFzd <fzd.sara@gmail.com>
2025-06-09 15:16:15 +0330

    update chart

--- script.js
+++ script.js
@@ -62,65 +62,86 @@ async function loadCoins() {
     }
 }
 loadCoins();
-
-
-async function updateChart(volume, symbol) {
-
-    const resolution = document.getElementById("resolution");
-    var resolutionVal = (resolution.value) * 86400;
-    const startDate = document.getElementById("startDate").value;
-    const sdt = DateTime.fromISO(startDate);
-    const sts = Math.floor(sdt.toSeconds());
-    const endDate = document.getElementById("endDate").value;
-    const edt = DateTime.fromISO(endDate);
-    const ets = Math.floor(edt.toSeconds());
-    const res = await fetch(`https://api.exir.io/v2/chart?symbol=${symbol}&resolution=${resolutionVal}&from=${sts}&to=${ets}`);
-    const data = await res.json();
-
-    console.log(data);
-
-}
-
-debugger;
 async function drawChart(symbol) {
 
-    var DateTime = luxon.DateTime;
-    now = DateTime.now();
-    later = now.minus({ days: 7 });
-    const nowToSeconds = Math.floor(now.toSeconds());
-    const laterToSeconds = Math.floor(later.toSeconds());
+    const interval = document.getElementById('resolution').value;
+    const resolution = interval + 'D';
+    const from = document.getElementById('startDate').value;
+    const to = document.getElementById('endDate').value;
 
-    const res = await fetch(`https://api.exir.io/v2/chart?symbol=${symbol}&resolution=1D&from=${laterToSeconds}&to=${nowToSeconds}`);
-    const data = await res.json();
-    const volumes = data.volume;
+    const userInput = {};
+    if (symbol) userInput.symbol = symbol;
+    if (resolution) userInput.resolution = resolution;
+    if (from) userInput.from = Math.floor(from.getTime() / 1000);
+    if (to) userInput.to = Math.floor(to.getTime() / 1000);
 
-    for (const item in volumes) {
-        const volume = volumes[item];
+    const defaultParams = {
+        symbol: 'btc-usdt',
+        resolution: "1D",
+        from: 1616987453,
+        to: 1619579513,
+    };
 
-        alert("send")
-        updateChart(volume, symbol);
+    const params = {
+        symbol: typeof userInput.symbol === 'string' ? userInput.symbol : defaultParams.symbol,
+        resolution: typeof userInput.resolution === 'string' ? userInput.resolution : defaultParams.resolution,
+        from: typeof userInput.from === 'number' ? userInput.from : defaultParams.from,
+        to: typeof userInput.to === 'number' ? userInput.to : defaultParams.to,
+    };
+
+    try {
+        const res = await fetch(`https://api.exir.io/v2/chart?symbol=${params.symbol}&resolution=${params.resolution}&from=${params.from}&to=${params.to}`);
+        const data = await res.json();
+
+        console.log(data);
+        const defaultSymbol = data.find(item => item.symbol === symbol);
+
+        updateChart(defaultSymbol);
+
+        // const fromObj = data.find(item => item.symbol === symbol && item.time === from);
+        // const toObj = data.find(item => item.symbol === symbol && item.time === to);
+
+        // if (!fromObj || !toObj) {
+        //     console.log('Could not find data.');
+        //     return;
+        // }
+
+        // const fromVolume = fromObj.volume;
+        // const toVolume = toObj.volume;
+
+        // updateChart(fromObj, toObj)
+
+
+    } catch (error) {
+        console.error('Error fetching chart data:', error);
     }
 }
-drawChart();
 
 
+async function updateChart(defaultSymbol) {
 
-const ctx = document.getElementById('myChart');
+    const toTime = Math.floor(defaultSymbol.time / 1000);
+    const fromTime = toTime - 7 * 86400;
 
-new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+
+    delete ctx;
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [fromTime, toTime],
+            datasets: [{
+                // label: '# of Votes',
+                data: [defaultSymbol.high, defaultSymbol.low],
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+}
